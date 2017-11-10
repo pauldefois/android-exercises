29b0573a45c981ce7ce2a19ffe01f37557f77c9a
Paul <defois.p@laposte.net>
2017-11-10 14:09:08 +0100

    correction exercice 10

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -3,6 +3,13 @@ package fr.android.androidexercises;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import timber.log.Timber;
 
 public class LibraryActivity extends AppCompatActivity {
@@ -16,10 +23,31 @@ public class LibraryActivity extends AppCompatActivity {
         Timber.plant(new Timber.DebugTree());
 
         // TODO build Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
         // TODO create a service
+        HenriPotierService service = retrofit.create(HenriPotierService.class);
 
         // TODO listBooks()
+        service.getBooks().enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+                for(Book book : response.body()) {
+                    Timber.d(book.getTitle());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Book>> call, Throwable t) {
+                Timber.e(t);
+            }
+        });
+
+
+
 
         // TODO enqueue call and display book title
 
@@ -27,5 +55,4 @@ public class LibraryActivity extends AppCompatActivity {
 
         // TODO display book as a list
     }
-
 }
